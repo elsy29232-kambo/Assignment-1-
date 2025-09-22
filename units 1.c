2dc40302245105edf4b0c0325418ce2960715734
elsy29232-kambo <elsy.29232@students.kyu.ac.ke>
2025-09-22 14:11:14 +0300

    Add files via upload

---
+++ units 1.c
@@ -0,0 +1,21 @@
+//program to display all units for this semester 
+
+/*
+Name :Elsy Kambo
+Reg No:PA106/29232/25
+Description : program to display all units for this semester 
+*/
+
+#include<stdio.h>
+
+int main()
+{
+printf("1.Mathematics for science \n");
+printf("2.Introduction to programming\n ");
+printf("3.Communication skills\n");
+printf("4.Discrete mathematics\n");
+printf("5.Computer systems and organisation\n");
+printf("6.Fundamentals of ICT\n");
+printf("7.Fundamentals of system analysis and design\n");
+return 0;
+}
